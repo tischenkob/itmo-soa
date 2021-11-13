20ccfcbbc1ae1fcbb396c9c192a7f838532d4482
Bogdan Tischenko <b.tishchenko@tinkoff.ru>
2021-11-13 20:21:50 +0300

    Обновил отлов ошибок

--- worker-service/src/main/java/ru/ifmo/ErrorHandler.java
+++ worker-service/src/main/java/ru/ifmo/ErrorHandler.java
@@ -8,23 +8,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import static javax.servlet.RequestDispatcher.*;
+import static javax.servlet.http.HttpServletResponse.*;
 
-@Slf4j
 @WebServlet("/error")
 public class ErrorHandler extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		Exception exception = (Exception) request.getAttribute(ERROR_EXCEPTION);
 		String message = (String) request.getAttribute(ERROR_MESSAGE);
 		String servletName = (String) request.getAttribute(ERROR_SERVLET_NAME);
-		log.info("Exception {} from servlet {}", message, servletName);
+		System.out.printf("ERROR: Exception '%s' from servlet '%s'", message, servletName);
 
 		if (exception instanceof IllegalArgumentException) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+			response.sendError(SC_BAD_REQUEST, message);
+		}
+
+		if (exception instanceof NoSuchElementException) {
+			response.sendError(SC_NOT_FOUND, message);
 		}
 	}
 }
